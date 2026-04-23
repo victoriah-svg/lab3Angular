3c7308387be6f2be200e87adc031e7e8f06b5a19
Victoria Helin <vickanhelin@hotmail.com>
2026-04-23 13:47:41 +0200

    importerat RouterLinkActive och lagt till styling för aktiv länk

--- src/app/main-nav/main-nav.ts
+++ src/app/main-nav/main-nav.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './main-nav.html',
   styleUrl: './main-nav.css',
 })
