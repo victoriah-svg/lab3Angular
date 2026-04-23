5b8ed608a400b99c7435d5b83cb0fde600bc1034
Victoria Helin <vickanhelin@hotmail.com>
2026-04-23 18:41:08 +0200

    lagt till metoder för konvertering från meter till feet och från feet till meter och databinding för att skicka data från input och visa resultat

--- src/app/convert/convert.ts
+++ src/app/convert/convert.ts
@@ -10,14 +10,27 @@ import { FormsModule } from '@angular/forms';
 export class Convert {
   inputC: number = 0;
   inputF: number = 0;
-  result: number = 0;
+  inputMet: number = 0;
+  inputFeet: number = 0; 
+  result1: number = 0;
+  result2: number = 0;
+  result3: number = 0;
+  result4: number = 0;
 
   //Metoder:
   farenheit(): void{
-    this.result = this.inputC * 9/5 + 32;
+    this.result1 = this.inputC * 9/5 + 32;
   }
 
   celsius(): void {
-    this.result = (this.inputF - 32) / 1.8;
+    this.result2 = (this.inputF - 32) / 1.8;
+  }
+
+  feet(): void {
+    this.result3 = this.inputMet * 3.28;
+  }
+
+  meter(): void {
+    this.result4 = this.inputFeet * 0.3048;
   }
 }
