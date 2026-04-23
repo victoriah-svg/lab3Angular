5770c6cbf51fdb50fe3964f5359d6f61f92665b7
Victoria Helin <vickanhelin@hotmail.com>
2026-04-23 18:02:39 +0200

    importerat FormsModule, lagt till input samt knapp i hmtl för convert samt metod för att konvertera celsius till farenheit i klass för convert

--- src/app/convert/convert.ts
+++ src/app/convert/convert.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.html',
   styleUrl: './convert.css',
 })
-export class Convert {}
+export class Convert {
+  inputC: number = 0;
+  result: number = 0;
+
+  //Metoder:
+  farenheit(): void{
+    this.result = this.inputC * 9/5 + 32;
+  }
+}
