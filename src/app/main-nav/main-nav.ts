b5cac17bcc282f96e6984b5f105ea9b1a42424ab
Victoria Helin <vickanhelin@hotmail.com>
2026-04-23 11:13:49 +0200

    lagt till navigeringsmeny som en komponent

---
+++ src/app/main-nav/main-nav.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-nav',
+  imports: [],
+  templateUrl: './main-nav.html',
+  styleUrl: './main-nav.css',
+})
+export class MainNav {}
