ef3f5a5592b577504223f08aa3f8f5445a2cdf56
Victoria Helin <vickanhelin@hotmail.com>
2026-04-23 11:10:39 +0200

    lagt till routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Convert } from './convert/convert';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: Home },
+    { path: "about", component: About },
+    { path: "convert", component: Convert},
+    { path: "", redirectTo: "home", pathMatch: "full"}
+];
